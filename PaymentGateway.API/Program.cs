cddf06e7c5cf92d56434b2bf1dbacfb9aaf52308
Ravi <ravi@eve.world>
2023-01-02 02:18:45 +0530

    Phase 1 implementation completed

--- PaymentGateway.API/Program.cs
+++ PaymentGateway.API/Program.cs
@@ -1,3 +1,11 @@
+using AutoWrapper;
+using Microsoft.EntityFrameworkCore;
+using PaymentGateway.API;
+using PaymentGateway.BLL.Interfaces;
+using PaymentGateway.BLL.Services;
+using PaymentGateway.DAL;
+using PaymentGateway.DAL.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,8 +15,19 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+// Custom service injection
+builder.Services.AddDbContext<DatabaseContext>(options=>options.UseInMemoryDatabase("PaymentDb"));
+builder.Services.AddTransient<IPaymentProcessor, PaymentProcessor>();
+builder.Services.AddTransient<ITransactionRepository, TransactionRepository>();
+builder.Services.AddTransient<IMerchantRepository, MerchantRepository>();
+builder.Services.AddTransient<IBankService, CKOBankService>();
+builder.Services.AddTransient<ICardValidator, CardValidator>();
+builder.Services.AddTransient<ICurrencyRepository, CurrencyRepository>();
 
+
+var app = builder.Build();
+SeedData.AddSeedData(app);
+app.UseApiResponseAndExceptionWrapper();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
